2b0c6d87e0d1ecb9b89347a8800a402b0a7a1ac6
Justo Prado <pradoamoretty@gmail.com>
2023-09-15 17:30:58 -0600

    agregando formulario de Netlify

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
 import Nav from './components/Nav.vue';
 import Bio from './partials/Bio.vue';
 import Skills from './components/Skills.vue';
-import Contact from './components/Contact.vue';
 import Top from './partials/Top.vue';
 </script>
 
 <template>
 
-  <Nav class=""/>
+  <Nav/>
 <Bio/>
 <Skills/>
-<Contact/>
 <Top class="fixed sm:inset-y-[580px] sm:inset-x-[1150px] inset-y-[600px] inset-x-[330px] "/>
 
 </template>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,21 +8,21 @@
                 <h1
                     class="text-black font-workSans font-semibold text-4xl pt-6 border-black w-auto h-auto text-center  mb-4">
                     Contacto</h1>
-                <label for="asunto">
+                
                     <input
                         class="h-[40px] w-[300px] mx-[40px] border-slate-500 border-b-[1px] bg-transparent  text-left  mb-3 focus:outline-none focus:border-none focus:ring-1 focus:ring-sky-400 focus:rounded-md text-black"
                         type="text" name="asunto" required placeholder="  Asunto">
-                </label>
-                <label for="email">
+                
+                
                     <input
                         class=" h-[40px] w-[300px] mx-[40px] border-slate-500 border-b-[1px] bg-transparent text-left  mb-3 focus:outline-none focus:border-none focus:ring-1 focus:ring-sky-400 focus:rounded-md text-black"
                         type="email" name="email" required placeholder="  Email">
-                </label>
-                <label for="message">
+                
+                
                     <textarea
                         class="h-[90px] w-[300px] mx-[40px] border-slate-500 border-b-[1px] bg-transparent  text-left mb-3 focus:outline-none focus:border-none focus:ring-1 focus:ring-sky-400 focus:rounded-md  text-black"
                         name="message" id="message" required cols="30" rows="10" placeholder="Dime algo."></textarea>
-                </label>
+                
                 <button type="submit"
                     class="text-black mx-[115px] mt-8 hover:bg-black hover:text-white w-[150px] h-[40px] bg-slate-200 rounded-lg">Submit</button>
             </form>
